b6932e0ed606548a48fd5f714150c039c6f7845f
rustproofFish <adrian.c.ward@gmail.com>
2016-02-07 15:43:04 +0000

    Empty view created

---
+++ ExampleView/ImageSearchTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageSearchTableViewCell.swift
+//  SwinjectMVVMExample
+//
+//  Created by Adrian Ward on 07/02/2016.
+//  Copyright © 2016 Adrian Ward. All rights reserved.
+//
+
+import UIKit
+
+internal final class ImageSearchTableViewCell: UITableViewCell {
+
+}
---
+++ ExampleView/ImageSearchTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ImageSearchTableViewController.swift
+//  SwinjectMVVMExample
+//
+//  Created by Adrian Ward on 07/02/2016.
+//  Copyright © 2016 Adrian Ward. All rights reserved.
+//
+
+import UIKit
+
+public final class ImageSearchTableViewController: UITableView {
+    
+}
